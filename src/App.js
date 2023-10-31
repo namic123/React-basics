9d6ebcf4bef8f1f09d2a34e0717ff109c07daf2f
jay sung <namic123@naver.com>
2023-10-31 14:25:03 +0900

    종료태그 작성 연습, 컴포넌트 여러번 사용 연습

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
+      <MyComponent1 />
+      <MyComponent1 />
     </div>
+  );
+}
+function MyComponent1() {
+  return (
+    <img
+      src="https://i.imgur.com/SuL4x4v_d.webp?maxwidth=520&shape=thumb&fidelity=high"
+      alt=""
+    />
   );
 }
 

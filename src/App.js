3f75a0ea1853532f9a0af57a1748f8955ef5ce32
jay sung <namic123@naver.com>
2023-10-31 14:08:41 +0900

    jsx에서 종료태그 꼭 작성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 function App() {
+  // jsx에서 태그 사용시 꼭 종료 태그를 작성해야 한다.
+  // 빈 요소이면 시작태그에서 종료
   return (
     <div>
       <h1>hello react</h1>
-        <h2>hi react</h2>
-        <MyComp />
+      <h2>hi react</h2>
+      <p>
+        Lorem <br /> Lorem ipsum dolor.
+      </p>
+      <MyComp />
     </div>
   );
 }
@@ -11,20 +16,20 @@ function App() {
 // Component명은 대문자로 시작
 // return문을 가져야함.
 // return 값은 보통 JSX 코드
-function MyComp(){
-    // return에 JSX 코드 작성 가능
-    // 꼭 하나의 root tag가 존재해야 한다.
-    // return 값이 여러 줄이면()로 감싸기
-    // root tag가 필요업다면 fragment 사용 가능
+function MyComp() {
+  // return에 JSX 코드 작성 가능
+  // 꼭 하나의 root tag가 존재해야 한다.
+  // return 값이 여러 줄이면()로 감싸기
+  // root tag가 필요업다면 fragment 사용 가능
 
-    // jsx에서 주석 사용법
-    // {/**/}
-    return (
-        <>
-            {/*fragment는 <></>*/}
-        <h1>HI Component</h1>
-        <h2>hello</h2>
-        </>
-    )
+  // jsx에서 주석 사용법
+  // {/**/}
+  return (
+    <>
+      {/*fragment는 <></>*/}
+      <h1>HI Component</h1>
+      <h2>hello</h2>
+    </>
+  );
 }
 export default App;

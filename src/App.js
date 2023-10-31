33c9d7101d24133b471cfc41bd1b5b3bb7ac44a0
jay sung <namic123@naver.com>
2023-10-31 14:54:27 +0900

    jsx에서 javascript쓰기 연습 완료

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ function MyComp() {
   const country = "korea";
   const phone = "01034234234";
   const age = 44;
+  const randomNumber = Math.random();
   return (
     <>
       <h1>age눈 {age}</h1>
       <h1> address는 {address}</h1>
       <h1>country는 {country}</h1>
       <h1>phone은 {phone}</h1>
+      <h1>랜덤 번호는 {Math.ceil(randomNumber * 10)}</h1>
     </>
   );
 }

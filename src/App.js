08b7a9f22b554f9e37a185a5f79ddd1148edf057
jay sung <namic123@naver.com>
2023-10-31 16:19:20 +0900

    jsx에서 인라인 스타일 적용하는 방법

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 function App() {
-  // jsx가 js코드로 변환됨
-  // 따라서 js 키워드를 사용할 수 없음
+  const myStyle = {
+    color: "tomato",
+    backgroundColor: "gold", // lowerCamelCase
+    fontSize: "70px",
+    textAlign: "center",
+  }; // js Object
+
   return (
     <>
-      {/*class 속성은 className으로 작성*/}
-      <div className="header">Lorem ipsum dolor</div>
-      <div className="error">Lorem ipsum dolor.</div>
-      {/*for 속성은 htmlFor로 작성*/}
-      <div>
-        <label htmlFor="nameInput">이름</label>
-        <input type="text" id="nameInput" />
-      </div>
-      <div>
-        <label htmlFor="ageInput"></label>
-        <input type="text" id="ageInput" />
+      {/* style 속성은 객체로 주어야 한다.*/}
+      <div style={myStyle}>Lorem ipsum dolor.</div>
+      <hr />
+      <div
+        style={{
+          color: "tomato",
+          backgroundColor: "skyblue", // lowerCamelCase
+          fontSize: "70px",
+          textAlign: "center",
+        }}
+      >
+        Lorem ipsum dolor.
       </div>
     </>
   );

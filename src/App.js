ab9c24956f6d147aa2f7a3803abcb48cd267ec6b
jay sung <namic123@naver.com>
2023-10-31 12:14:45 +0900

    component 연습

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 function App() {
   return (
     <div>
-
+      <MyComponent1/>
+      {/*
+      div
+      h1 Hello Component
+      div/
+      */}
+      <MyComponent2/>
+      {/*
+      <div>
+        <h1>Hello Component</h1>
+      </div>
+      */}
     </div>
   );
+}
+function MyComponent1(){
+  return (
+      <div>
+        <h1>Hello Component</h1>
+      </div>
+  )
+}
+function MyComponent2(){
+  return (
+      <>
+      <p>lorem ipsum alkdjf alkdjf </p>
+    <ul>
+        <li>lorem</li>
+        <li>ipsum</li>
+        <li>dolor</li>
+    </ul>
+    </>
+  )
 }
 
 export default App;

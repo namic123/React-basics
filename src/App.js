e82d4904454765c6cbde7a21cf75596e7986d64d
jay sung <namic123@naver.com>
2023-10-31 16:33:18 +0900

    jsx에서 인라인 스타일 적용하는 방법

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 function App() {
+  let divStyle = {
+    display: "flex",
+    justifyContent: "space-around",
+  };
   return (
-    <div>
-
+    <div style={divStyle}>
+      <MyComp />
+      <MyComp />
+      <MyComp />
     </div>
   );
 }
 
+function MyComp() {
+  const name = "흥민";
+  return <div style={{ backgroundColor: "tomato" }}>{name}</div>;
+}
 export default App;

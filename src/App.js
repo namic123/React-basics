00cd8722119c2c1b81ad4b3956d4abb65fa24641
jay sung <namic123@naver.com>
2023-10-31 14:52:36 +0900

    jsx에서 javascript쓰기 연습

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 function App() {
   return (
     <div>
-
+      <MyComp />
     </div>
   );
 }
-
+function MyComp() {
+  const address = "seoul";
+  const country = "korea";
+  const phone = "01034234234";
+  const age = 44;
+  return (
+    <>
+      <h1>age눈 {age}</h1>
+      <h1> address는 {address}</h1>
+      <h1>country는 {country}</h1>
+      <h1>phone은 {phone}</h1>
+    </>
+  );
+}
 export default App;

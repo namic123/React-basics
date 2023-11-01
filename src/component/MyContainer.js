45d830bbecf687a08f6ae7c75faa56913dde57a3
jay sung <namic123@naver.com>
2023-11-01 14:27:13 +0900

    export default import 연습 완료

---
+++ src/component/MyContainer.js
@@ -0,0 +1,13 @@
+export function MyBox() {
+  return (
+    <>
+      <div>
+        <h1>MyBox이다</h1>
+      </div>
+    </>
+  );
+}
+export const person = { name: "jay", address: "인천" };
+export const team = { title: "맨유", score: 100 };
+
+export default MyBox;

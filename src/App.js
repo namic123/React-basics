2c751b50777ad89c24f966385271f1d1d91115de
jay sung <namic123@naver.com>
2023-11-01 11:35:32 +0900

    props 구조분해할당, 기본값 주기 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 function App() {
   return (
     <div>
-
+      <MyComp
+        m={"30px"}
+        p={"10px"}
+        color={"gold"}
+        bg={"black"}
+        text={"hello react"}
+      />
+      <MyComp m={"30px"} bg={"black"} />
+      <MyComp m={"30px"} bg={"black"} text={"hello js"} />
+      <MyComp p={"10px"} color={"gold"} text={"hello css"} />
     </div>
+  );
+}
+function MyComp({
+  m = "60px",
+  p = "30px",
+  color = "green",
+  bg = "tomato",
+  text = "값이 비어있음",
+}) {
+  return (
+    <>
+      <div style={{ color: color, margin: m, padding: p, backgroundColor: bg }}>
+        <h1>{text}</h1>
+      </div>
+    </>
   );
 }
 

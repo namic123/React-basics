f81f6508208f062ca8501952883ad6089a5cc76e
jay sung <namic123@naver.com>
2023-11-01 09:33:53 +0900

    props: 부모 컴포넌트로부터 전달받은 데이터

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 function App() {
-  const myStyle = {
-    color: "tomato",
-    backgroundColor: "gold", // lowerCamelCase
-    fontSize: "70px",
-    textAlign: "center",
-  }; // js Object
-
   return (
     <>
-      {/* style 속성은 객체로 주어야 한다.*/}
-      <div style={myStyle}>Lorem ipsum dolor.</div>
-      <hr />
-      <div
-        style={{
-          color: "tomato",
-          backgroundColor: "skyblue", // lowerCamelCase
-          fontSize: "70px",
-          textAlign: "center",
-        }}
+      <h1
+        className="note"
+        style={{ backgroundColor: "blue", color: "white" }}
+        title="제목 요소"
       >
-        Lorem ipsum dolor.
-      </div>
+        Lorem.
+      </h1>
+      <p title="문단 요소">Lorem ipsum dolor.</p>
+      <MyComp title="내 컴포넌트" name={"흥민"} />
+      <MyComp title="I am 컴포넌트" name={"강인"} />
     </>
   );
 }
+// 함수 인자로
+// 사용한 곳에서 넘긴 property들이 객체형태로 담김
+// 예) title="내 컴포넌트" name={"흥민"} => {title: "내 컴포넌트", name:"흥민"}
 
+function MyComp(props) {
+  console.log("MyComp가 받은 property는 ?", props.title, "name은", props.name);
+  return (
+    <div>
+      <h1 title={props.title}>hello {props.name}</h1>
+    </div>
+  );
+}
 export default App;

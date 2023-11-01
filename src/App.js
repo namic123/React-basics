c602b98f6e657b14e34b9dc2eb920c86690fb5ce
jay sung <namic123@naver.com>
2023-11-01 10:12:09 +0900

    props 작성, 활용연습 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 function App() {
   return (
     <div>
-
+      <MyText
+        style={{ color: "tomato", backgroundColor: "gold" }}
+        value={"1번째"}
+      />
+      <MyText
+        style={{ color: "green", backgroundColor: "skyblue" }}
+        value={"2번째"}
+      />
     </div>
   );
 }
-
+function MyText(props) {
+  return (
+    <>
+      <div style={props.style}>
+        <p>{props.value}</p>
+      </div>
+    </>
+  );
+}
 export default App;

111c1ee73d4bfa96e112f0ba51c7044513049d60
jay sung <namic123@naver.com>
2023-11-01 15:47:52 +0900

    보통의 css 사용시 클래스명이 중복될 수 있음.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 
 import { MyComp } from "./component/MyComp";
 import { MyBox } from "./component/MyBox";
+import { MyContainer } from "./component/MyContainer";
 
 function App() {
   return (
     <>
       <MyComp />
       <MyBox />
+      <MyContainer />
     </>
   );
 }
---
+++ src/component/MyContainer.js
@@ -0,0 +1,10 @@
+import "../style/style2.css";
+export function MyContainer() {
+  return (
+    <>
+      <div className="boxClass">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+      </div>
+    </>
+  );
+}

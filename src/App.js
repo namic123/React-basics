711b604bdee3f33293ea74519919d9c90adbc0b9
jay sung <namic123@naver.com>
2023-11-01 11:13:31 +0900

    props destructuring assignment 구조분해 할당, 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 function App() {
   return (
     <div>
-
+      <MyBox
+        p={"10px"}
+        m={"10px"}
+        color={"tomato"}
+        text={"1번 째 "}
+        bg={"green"}
+      />
+      <MyBox
+        p={"50px"}
+        m={"50px"}
+        color={"gold"}
+        text={"2번 째 "}
+        bg={"blue"}
+      />
+      <MyBox
+        p={"30px"}
+        m={"30px"}
+        color={"skyblue"}
+        text={"3번 째 "}
+        bg={"black"}
+      />
     </div>
   );
 }
-
+function MyBox({ p, m, bg, color, text }) {
+  return (
+    <>
+      <div style={{ padding: p, margin: m, color: color, backgroundColor: bg }}>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
 export default App;

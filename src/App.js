9f1c5237e5d1dc8ebab9c699f2ec7c9a1e4222b9
jay sung <namic123@naver.com>
2023-11-01 11:22:15 +0900

    props 구조분해할당, 기본값 주기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 function App() {
   return (
     <>
-      <MyComp value={"흥민"} address="신촌" />
-      <MyComp value={"강인"} address="강남" />
+      <MyComp name={"흥민"} address={"신촌"} age={33} city={"서울"} />
+      <MyComp name={"강남"} age={22} city={"부산"} />
+      <MyComp age={42} city={"부산"} />
+      <MyComp name="" age={23} city="" address="" />
     </>
   );
 }
 
-function MyComp(props) {
-  // 구조분해 할당
-  // function MyComp({value, address}) 이렇게 작성도 가능
-  // 구조 분해 할당 방식이 더 많이 ㅏ용됨
-  let { value, address } = props;
-  // value = props.value
-  // address = props.value
+function MyComp({ name = "홍길동", age = 100, address = "?", city = "?" }) {
+  // 기본값을 줄 수 있음
+
   return (
     <>
       <div>
-        <h1>
-          {value}는 {address}에 산다.
-        </h1>
+        <p>
+          {name}은 {age}세 이고 {address}, {city}에 산다
+        </p>
       </div>
     </>
   );

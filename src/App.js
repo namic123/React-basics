34f04b5c60d416e520ba498c93bde644d0ea3f44
jay sung <namic123@naver.com>
2023-11-01 10:46:15 +0900

    props destructuring assignment 구조분해 할당, 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 function App() {
   return (
     <div>
-
+      <MyElem name={"흥민"} age={44} team={"토트넘"} />
+      <MyElem name={"강인"} age={33} team={"파리"} />
     </div>
   );
 }
-
+function MyElem({ name, age, team }) {
+  return (
+    <>
+      <div>
+        <h1>
+          이름은 {name} 나이는 {age} 팀은 {team}
+        </h1>
+      </div>
+    </>
+  );
+}
 export default App;

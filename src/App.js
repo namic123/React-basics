8b01abe9df2a3194bb0f5c8409f686d3796d2b16
jay sung <namic123@naver.com>
2023-11-01 14:01:21 +0900

    한 파일에서 여러개의 값(객체, 값, 함수) export, import 가능 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import {
+  country,
+  MyContent,
+  MyElem,
+  MyNavBar,
+  text,
+  value,
+} from "./component/MyElem";
+
 function App() {
+  // 아래 컴포넌트를 하나의 파일에 작성하고 export하기
+  // 파일명은 component/MyElem.js
   return (
     <div>
-
+      <p>{country}</p>
+      <p>{text}</p>
+      <p>{value}</p>
+      <MyElem />
+      <MyNavBar />
+      <MyContent />
     </div>
   );
 }
---
+++ src/component/MyElem.js
@@ -0,0 +1,17 @@
+let country = "나라";
+let text = "텍스트 ";
+let value = "값";
+export { country, text };
+export { value };
+export function MyElem() {
+  return null;
+}
+export function MyNavBar() {
+  return null;
+}
+
+function MyContent() {
+  return null;
+}
+
+export { MyContent };

f3e0e941c1054eb9be6c165a7bde42a08d6f0a47
jay sung <namic123@naver.com>
2023-11-03 14:24:39 +0900

    e.stopPropagation(); 활용 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { Button, Center } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  function handleOuterBoxClick(e) {
+    e.stopPropagation();
+    console.log("바깥 상자 클릭됨");
+  }
+  function handleinnerBoxClick(e) {
+    e.stopPropagation();
+    console.log("안쪽 상자 클릭됨");
+  }
+  function handleBtnClick(e) {
+    e.stopPropagation();
+    console.log("클릭됨");
+  }
+  return (
+    <>
+      <Center bg={"red"} w={"200px"} h={"200px"} onClick={handleOuterBoxClick}>
+        <Center
+          bg={"gold"}
+          w={"100px"}
+          h={"100px"}
+          onClick={handleinnerBoxClick}
+        >
+          <Button bg={"green"} w={"50px"} h={"50px"} onClick={handleBtnClick}>
+            Button
+          </Button>
+        </Center>
+      </Center>
+    </>
+  );
 }
 
 export default App;

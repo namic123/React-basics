41b970a93b1d67f56f22cb8350526cc62b61c7e6
jay sung <namic123@naver.com>
2023-11-03 12:33:58 +0900

    event.preventDefault(): 기본 기능 취소 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <></>;
+  function handleClick(e) {
+    e.preventDefault();
+    window.location.href = "https://www.naver.com";
+  }
+  return (
+    <>
+      <a href="https://www.daum.net" onClick={handleClick}>
+        다음으로 이동
+      </a>
+    </>
+  );
 }
 
 export default App;

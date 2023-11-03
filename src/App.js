b15d4a7c642e78adb37736f0fef9feeafcc8bf65
jay sung <namic123@naver.com>
2023-11-03 09:04:30 +0900

    필요없는 파일들 삭제

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-// style 적용 방법
-
-import { MyComp } from "./component/MyComp";
-import { MyBox } from "./component/MyBox";
-import { MyContainer } from "./component/MyContainer";
-
 function App() {
-  return (
-    <>
-      <MyComp />
-      <MyBox />
-      <MyContainer />
-    </>
-  );
+  return <></>;
 }
 
 export default App;

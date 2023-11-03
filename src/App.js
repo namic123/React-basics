8f96e2e2c5723bc3f32e8b5e913c4d8c86ebe77b
jay sung <namic123@naver.com>
2023-11-03 11:50:33 +0900

    변수의 값이 변경 되어도 바로 dom에 반영되지 않는다.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Input } from "@chakra-ui/react";
 
 function App(props) {
+  let text = "initial text";
   function handleChange(e) {
     // console.log("인풋값이 변경!!");
     console.log(e.target.value);
+    text = e.target.value; // 변수의 값이 변경 되어도 바로 dom에 반영되지 않는다.
+    console.log("text", text); // 콘솔을 찍었을 때는 값이 변경되어 있는 것으로 보임
   }
   return (
     <div>
+      <p>{text}</p>
       <Input onChange={handleChange} />
       <Input onChange={handleChange} />
     </div>
--- study-recap/event.md
+++ study-recap/event.md
@@ -5,3 +5,19 @@
 ### event.target
 * 이벤트가 실제로 발생한 DOM요소를 가리킨다.
 * 이벤트 리스너에 의해 캐치된 가장 구체적인 곳의 요소를 참조한다.
+* 이것은 이벤트 위임이나 이벤트가 발생한 위치를 파악하고자 할 때 유용
+```js
+function List() {
+  function handleClick(event) {
+    console.log(event.target); // 클릭된 요소를 콘솔에 출력합니다.
+  }
+
+  return (
+    <ul onClick={handleClick}>
+      <li>항목 1</li>
+      <li>항목 2</li>
+      <li>항목 3</li>
+    </ul>
+  );
+}
+```

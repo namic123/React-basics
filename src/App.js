8bf56bd0af383f187ffc1970e722843efa1f6d22
jay sung <namic123@naver.com>
2023-11-03 10:50:02 +0900

    함수를 props로 전달하는 여러가지 방법 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Button } from "@chakra-ui/react";
+
+function MyComp({ children, executeClick }) {
+  return <Button onClick={executeClick}>{children}</Button>;
+}
 function App() {
-  return <></>;
+  function func1() {
+    console.log("func1 실행");
+  }
+
+  let func2 = () => {
+    console.log("arrow function 실행 111");
+  };
+
+  return (
+    <div>
+      <MyComp executeClick={func1}>Button1</MyComp>
+      <MyComp executeClick={func2}>Button2</MyComp>
+      <MyComp
+        executeClick={() => {
+          console.log("arrow function 실행 222");
+        }}
+      >
+        Button3
+      </MyComp>
+      <MyComp executeClick={() => console.log("arrow function 실행333")}>
+        Button4
+      </MyComp>
+    </div>
+  );
 }
 
 export default App;

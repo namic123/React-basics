b3f3207ab2d49b3ef61fee901ca58364bcb01e3a
jay sung <namic123@naver.com>
2023-11-03 10:31:22 +0900

    함수를 prop으로 전달, 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Button } from "@chakra-ui/react";
+
+function MyButton({ excuteClick, children }) {
+  return <Button onClick={excuteClick}>{children}</Button>;
+}
 function App() {
-  return <></>;
+  function func1() {
+    console.log("hello");
+  }
+  function func2() {
+    console.log("greeting");
+  }
+  return (
+    <>
+      <MyButton excuteClick={func1}>Hello</MyButton>
+      <MyButton excuteClick={func2}>Greeting</MyButton>
+    </>
+  );
 }
 
 export default App;

9a45dbd911e21c060548fa514f2dc99260a3f042
jay sung <namic123@naver.com>
2023-11-03 10:21:10 +0900

    함수를 prop으로 전달

--- src/App.js
+++ src/App.js
@@ -1,59 +1,26 @@
 import React from "react";
-import {
-  Box,
-  Button,
-  Flex,
-  Input,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Text,
-} from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Button } from "@chakra-ui/react";
 
-function App(props) {
+function MyComp({ to, someProp }) {
+  someProp();
   return (
-    <Flex
-      px={{ base: "0px", md: "70px", lg: "120px", xl: "180px" }}
-      justify="space-between"
-      py="8px"
-      my="10px"
-      bg="gray.100"
-    >
-      <Flex gap="7px" align="center">
-        <Text as="b">Navbar</Text>
-        <Button variant={"ghost"}>Home</Button>
-        <Button variant={"ghost"}>Link</Button>
-
-        <Menu>
-          <MenuButton
-            variant="ghost"
-            as={Button}
-            rightIcon={<ChevronDownIcon />}
-          >
-            Dropdown
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Download</MenuItem>
-            <MenuItem>Create a Copy</MenuItem>
-            <MenuItem>Mark as Draft</MenuItem>
-            <MenuItem>Delete</MenuItem>
-            <MenuItem>Attend a Workshop</MenuItem>
-          </MenuList>
-        </Menu>
-
-        <Button variant={"ghost"} isDisabled={true}>
-          Disabled
-        </Button>
-      </Flex>
-      <Flex gap="7px" align="center">
-        <Input size="sm" bg="white" colorScheme="green" />
-        <Button size="sm" variant="outline" colorScheme="green">
-          Search
-        </Button>
-      </Flex>
-    </Flex>
+    <>
+      <a href={to[0]}>link!!!</a>
+      <Button onClick={someProp}>클릭</Button>
+    </>
+  );
+}
+function App(props) {
+  function func1() {
+    console.log("func1 실행됨");
+  }
+  return (
+    <>
+      <MyComp to={["https://www.naver.com"]} someProp={func1}>
+        네이버로 이동
+      </MyComp>
+      >
+    </>
   );
 }
 export default App;

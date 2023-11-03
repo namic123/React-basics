e881171e7517153a2039fae1962ccc74d5d3813d
jay sung <namic123@naver.com>
2023-11-03 11:44:37 +0900

    event 객체 활용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import { Button } from "@chakra-ui/react";
+import React from "react";
+import { Input } from "@chakra-ui/react";
 
 function App(props) {
-  // 브라우저는 이벤트 핸들러 메소드에
-  // event 객체를 매개값으로 넣어줌
-  function handleClick(event) {
-    // console.log(event);
-    // console.log(event.target);
-    console.log(event.target.value);
-    // console.log(event.target.className); // 요소의 클래스명
-    // console.log(event.target.type); // 요소의 type - 여기서는 Button
+  function handleChange(e) {
+    // console.log("인풋값이 변경!!");
+    console.log(e.target.value);
   }
   return (
-    <>
-      <Button colorScheme="red" onClick={handleClick} value={"버튼1"}>
-        button1
-      </Button>
-      <Button colorScheme="blue" onClick={handleClick}>
-        button2
-      </Button>
-    </>
+    <div>
+      <Input onChange={handleChange} />
+      <Input onChange={handleChange} />
+    </div>
   );
 }
+
 export default App;

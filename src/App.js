2be9eba2d631f207196d1c5201f5ef42967c52f9
jay sung <namic123@naver.com>
2023-11-03 15:47:34 +0900

    예제)state를 활용하여 연산하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import React, { useState } from "react";
+import { Input } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+
+  let result = number1 + number2;
+  function handleNumber1(e) {
+    setNumber1(Number(e.target.value));
+  }
+  function handleNumber2(e) {
+    setNumber2(Number(e.target.value));
+  }
+
+  return (
+    <>
+      <div>
+        <Input type="number" onChange={handleNumber1} /> +
+        <Input type="number" onChange={handleNumber2} />={result}
+      </div>
+    </>
+  );
 }
 
 export default App;

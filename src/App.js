2863125fd27f4451203e8e4183ce8518ce3da793
jay sung <namic123@naver.com>
2023-11-03 12:23:46 +0900

    event.preventDefault(): 브라우저 기본기능 취소

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
-import { Input } from "@chakra-ui/react";
 
 function App(props) {
-  let text = "initial text";
-  function handleChange(e) {
-    // console.log("인풋값이 변경!!");
-    console.log(e.target.value);
-    text = e.target.value; // 변수의 값이 변경 되어도 바로 dom에 반영되지 않는다.
-    console.log("text", text); // 콘솔을 찍었을 때는 값이 변경되어 있는 것으로 보임
+  function handleClick(e) {
+    // 브라우저가 해야하는 기본 기능을 취소함(prevent)
+    e.preventDefault();
+    console.log("다른 일을 시킴");
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("다른 일을 시킴");
   }
   return (
     <div>
-      <p>{text}</p>
-      <Input onChange={handleChange} />
-      <Input onChange={handleChange} />
+      <a href="https://www.naver.com" onClick={handleClick}>
+        네이버
+      </a>
+      <div>
+        <form action="https://search.daum.net/search" onSubmit={handleSubmit}>
+          <input type="text" name="q" />
+          <button>검색</button>
+        </form>
+      </div>
     </div>
   );
 }

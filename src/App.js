fb5e14a351887faf42d0bbde86bf3212a60a4767
jay sung <namic123@naver.com>
2023-11-06 11:28:35 +0900

    state의 불변성, useState객체 리렌더링 방법 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Box, Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [message, setMessage] = useState("");
+  const [obj, setObj] = useState({ message: "" });
+
+  function handleObjectMessageChange(e) {
+    /*    const newObj = { ...obj };
+    newObj.message = e.target.value;
+    setObj(newObj);*/
+
+    // 리팩토링
+    setObj({ ...obj, message: e.target.value });
+  }
+
+  return (
+    <>
+      <div>
+        <Box>
+          <Input value={message} onChange={(e) => setMessage(e.target.value)} />
+          <Text>{message}</Text>
+        </Box>
+        <Box>
+          <Input value={obj.message} onChange={handleObjectMessageChange} />
+          <Text>{obj.message}</Text>
+        </Box>
+      </div>
+    </>
+  );
 }
 
 export default App;

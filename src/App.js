7dd0a97d727a17fcb49dc74639227778fac8b23b
jay sung <namic123@naver.com>
2023-11-06 15:14:23 +0900

    배열 리스트의 요소 삭제하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import { Box, Button, Input, ListItem, UnorderedList } from "@chakra-ui/react";
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [text, setText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleButtonClick() {
+    setItems([...items, text]);
+  }
+
+  function handleRemoveButtonClick(index) {
+    const nextItems = [...items];
+    nextItems.splice(index, 1);
+    setItems(nextItems);
+  }
+
+  return (
+    <>
+      <Box>
+        <Input value={text} onChange={(e) => setText(e.target.value)}></Input>
+        <Button onClick={handleButtonClick} colorScheme="blue">
+          추가
+        </Button>
+      </Box>
+      <Box>
+        <UnorderedList>
+          {items.map((item, index) => (
+            <ListItem key={index}>
+              {item}
+              <Button
+                colorScheme="red"
+                onClick={() => handleRemoveButtonClick(index)}
+              >
+                지우기
+              </Button>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
+    </>
+  );
 }
 
 export default App;

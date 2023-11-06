6be00ce25a2b41118b2e6106da9099c319da6aed
jay sung <namic123@naver.com>
2023-11-06 12:23:21 +0900

    배열을 jsx에서 사용, 리스트아이템에 매핑되는 컴포넌트는 key속성을 가져야한다.
    keyprop은 유일성을 가져야하며, 주로 db의 PK가 사용될 것이다.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import { List, ListItem } from "@chakra-ui/react";
 
 function App(props) {
   const arr = ["pizza", "돈까스", "햄버거", "김치찌개"];
-  let listItems = arr.map((item) => <ListItem>{item}</ListItem>);
+  let listItems = arr.map((item, index) => (
+    <ListItem key={index}>{item}</ListItem>
+  ));
   const arr2 = ["a", "b", "c"];
   const playerArr = ["손흥민", "이강인", "김민재"];
-
+const arr4 = {
+  {id:0, name:"라떼"},
+  {id:1, name:"피자"},
+  {id:2, name:"치킨"},
+}
   return (
     <div>
       <List>{listItems}</List>
       <List>
-        {playerArr.map((item) => (
-          <ListItem>{item}</ListItem>
+        {playerArr.map((item, index) => (
+          <ListItem key={index}>{item}</ListItem>
         ))}
       </List>
       <List>
-        {arr2.map((item) => (
-          <ListItem>{item}</ListItem>
+        {arr2.map((item, index) => (
+          <ListItem key={index}>
+            {item},{index}
+          </ListItem>
         ))}
       </List>
     </div>

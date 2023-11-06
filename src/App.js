3013dcd9fffba7aa82eb72fd61bb1589b161c8b8
jay sung <namic123@naver.com>
2023-11-06 10:29:58 +0900

    hook의 개념, 규칙

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, { useState } from "react";
-import { Box, Button, Text } from "@chakra-ui/react";
 
 function App(props) {
-  const [number, setNumber] = useState(0);
-  const index = number % 3;
-  console.log(number);
-  return (
-    <div>
-      <Button onClick={() => setNumber(number + 1)}>다음</Button>
-      <Box borderWidth={"3px"} borderColor={index == 0 ? "blue" : "black"}>
-        <Text>message1</Text>
-      </Box>
-      <Box borderWidth={"3px"} borderColor={index == 1 ? "blue" : "black"}>
-        <Text>message2</Text>
-      </Box>
-      <Box borderWidth={"3px"} borderColor={index == 2 ? "blue" : "black"}>
-        <Text>message3</Text>
-      </Box>
-    </div>
-  );
+  //hook: use로 시작하는 함수
+  // hook은 컴포넌트 상단에 작성해야 한다.
+  // 조건문이나, 반복문, 중첩된 함수 안에 작성하면 안된다.
+
+  if (true) {
+    // hook 호출 코드 작성 불가
+  }
+  for (let i = 0; i < 3; i++) {
+    // hook 호출 코드 작성 불가
+  }
+  return <div></div>;
 }
 
 export default App;

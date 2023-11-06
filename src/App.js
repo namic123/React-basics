149425fecedac01b4ca7daa0207258186c55a921
jay sung <namic123@naver.com>
2023-11-06 09:38:48 +0900

    state활용 -(증감 버튼)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Button } from "@chakra-ui/react";
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [number, setNumber] = useState(0);
+  function handleNumberIncrease() {
+    setNumber(number + 1);
+  }
+  function handleNumberDecrease() {
+    setNumber(number - 1);
+  }
+  return (
+    <>
+      <h1>{number}</h1>
+      <Button onClick={handleNumberIncrease}>증가</Button>
+      <Button onClick={handleNumberDecrease}>감소</Button>
+    </>
+  );
 }
 
 export default App;

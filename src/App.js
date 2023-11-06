c74231abcbd8aa8b16227fe6ce2ba0d394d97a35
jay sung <namic123@naver.com>
2023-11-06 14:22:32 +0900

    state 배열 예제 - 리스트에 값 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { Box, Button, List, ListItem, OrderedList } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  const [items, setItems] = useState([]);
+  function handleButtonClick(item) {
+    // const newItems = [...items];
+    // newItems.push(item);
+
+    // 리팩토링 1
+    // const newItems = [...items, item];
+    // setItems(newItems);
+
+    // 리팩토링 2
+    setItems([...items, item]);
+  }
+  return (
+    <>
+      {/*Button[onClick]*5>lorem1*/}
+      <Button onClick={() => handleButtonClick("커피")}>커피</Button>
+      <Button onClick={() => handleButtonClick("케잌")}>케잌</Button>
+      <Button onClick={() => handleButtonClick("아이스크림")}>
+        아이스크림!
+      </Button>
+      <Button onClick={() => handleButtonClick("크로아상")}>크로아상</Button>
+      <Button onClick={() => handleButtonClick("단팥빵")}>단팥빵</Button>
+
+      <Box>
+        <OrderedList>
+          {items.map((item, index) => (
+            <ListItem key={index}>{item}</ListItem>
+          ))}
+        </OrderedList>
+      </Box>
+    </>
+  );
 }
 
 export default App;

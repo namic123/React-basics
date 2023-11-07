373c96cfa6e3c6aec90bc2c2a585718bfa83701c
jay sung <namic123@naver.com>
2023-11-07 14:14:39 +0900

    Context value에 state와 setState 활용 방법,

--- src/App.js
+++ src/App.js
@@ -1,51 +1,30 @@
 import React, { createContext, useContext, useState } from "react";
 import { Button, Text } from "@chakra-ui/react";
 
-function CComp() {
-  const message = useContext(MessageContext);
+function AComp() {
+  const context = useContext(TextContext);
   return (
-    <>
-      <Text>받은 메세지 : {message}</Text>
-    </>
+    <Button onClick={() => context.setMessage("변경 상태")}>
+      메시지 변경!
+    </Button>
   );
 }
 
 function BComp() {
-  return (
-    <>
-      <CComp />
-    </>
-  );
-}
-
-function AComp() {
-  return (
-    <>
-      <BComp />
-    </>
-  );
+  const { message } = useContext(TextContext); // 구조분해 할당
+  return <Text>{message}</Text>;
 }
 
 function App(props) {
-  const [message, setMessage] = useState("");
-
-  // message state를 CComp에 전달하기
-  // 1. context 만들기: createContext();
-  // 2. context에 state 넣기: <Context.Provider value={state}></Context.Provider>
-  // 3. tree(하위 컴포넌트)안에 사용하기
-
+  const [message, setMessage] = useState("초기 상태");
   return (
     <div>
-      <Button onClick={() => setMessage("바꾼 메세지!!")}>버튼</Button>
-      <MessageContext.Provider value={message}>
+      <TextContext.Provider value={{ message, setMessage }}>
         <AComp />
-      </MessageContext.Provider>
+        <BComp />
+      </TextContext.Provider>
     </div>
   );
 }
-
-// 1.Context 만들기
-// context 이름은 ...Context
-// 변수명은 대문자로 시작하고 Context로 끝남.
-const MessageContext = createContext(null);
+const TextContext = createContext(null);
 export default App;

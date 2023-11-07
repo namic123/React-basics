60ac0e1f1307c71369169c27b96dce48bec5084e
jay sung <namic123@naver.com>
2023-11-07 11:24:51 +0900

    lift state up 활용 2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
 import React, { useState } from "react";
+import * as PropTypes from "prop-types";
 import { Box, Input, Text } from "@chakra-ui/react";
 
-function MyInput(props) {
+function MyInput({ value, onChange }) {
   return (
-    <>
-      <Input
-        value={props.address}
-        onChange={(e) => props.onChange(e.target.value)}
-      />
-    </>
+    <Box>
+      <Input value={value} onChange={(e) => onChange(e.target.value)} />
+    </Box>
   );
 }
 
-function MyText(props) {
+function MyText({ text }) {
   return (
-    <>
-      <Box>
-        <Text>{props.address}</Text>
-      </Box>
-    </>
+    <Box>
+      <Text>{text}</Text>
+    </Box>
   );
 }
 
 function App(props) {
   const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
 
   return (
     <div>
-      <MyInput address={address} onChange={(text) => setAddress(text)} />
-      <MyText address={address} />
+      <MyInput value={address} onChange={(text) => setAddress(text)} />
+      <MyText text={address} />
+      <hr />
+      <MyInput value={email} onChange={(text) => setEmail(text)} />
+      <MyText text={email} />
     </div>
   );
 }

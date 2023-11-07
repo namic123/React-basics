fc098c57dae6588734fbe290e34f3e8de3efad92
jay sung <namic123@naver.com>
2023-11-07 15:49:20 +0900

    useEffect, axios활용하여 서버에서 전달해준 데이터 출력 예제

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App(props) {
+  const [myState, setMyState] = useState("");
   useEffect(() => {
     axios
       .get("/api/main1/sub1")
-      .then((response) => response.data)
-      .then((data) => console.log(data))
+      .then((response) => response.data) // 응답 데이터를 가져옴.
+      .then((data) => setMyState(data)) // 응답 데이터의 상태 저장
       .catch((error) => console.log(error));
   }, []);
-  return <div></div>;
+
+  return <div>{myState}</div>;
 }
 
 export default App;

b1109c472f40979ba868c23b66486e5918a545a4
jay sung <namic123@naver.com>
2023-11-07 16:37:47 +0900

    ajax 사용, DB의 데이터를 가져와서 렌더링

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ function App(props) {
   // get요청 /api/main1/sub2
   // 받은 값으로 message state 업데이트
 
-  const [message, setMessage] = useState("");
+  const [customer, setCustomer] = useState({});
   useEffect(() => {
     axios
-      .get("/api/main1/sub2")
+      .get("/api/main1/sub3")
       .then((response) => response.data)
-      .then((data) => setMessage(data))
+      .then((data) => setCustomer(data))
       .catch((error) => console.log(error));
   }, []);
   return (
     <div>
-      <h1>{message}</h1>
+      <h1>{customer.name}</h1>
+      <h1>{customer.id}</h1>
+      <h1>{customer.contactName}</h1>
+      <h1>{customer.address}</h1>
+      <h1>{customer.city}</h1>
+      <h1>{customer.postalCode}</h1>
+      <h1>{customer.country}</h1>
     </div>
   );
 }

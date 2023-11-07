c0a10a2bd2c83962600860cd9a57c55f6b7dbfb3
jay sung <namic123@naver.com>
2023-11-07 14:01:32 +0900

    Context- Prop drilling의 문제점 보완

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { Button, Text } from "@chakra-ui/react";
 
-function ChildComp(props) {
+function CComp() {
+  const message = useContext(MessageContext);
   return (
     <>
-      <Button onClick={props.onClick}>클릭!!</Button>
+      <Text>받은 메세지 : {message}</Text>
     </>
   );
 }
 
-function SomeComp(props) {
+function BComp() {
   return (
     <>
-      <ChildComp onClick={props.onClick} />
+      <CComp />
     </>
   );
 }
 
-function OtherChildComp(props) {
+function AComp() {
   return (
     <>
-      <Text>{props.text}</Text>
-    </>
-  );
-}
-
-function OtherComp(props) {
-  return (
-    <>
-      <OtherChildComp text={props.text} />
+      <BComp />
     </>
   );
 }
 
 function App(props) {
-  const [message, setMessage] = useState("원래 메세지!");
+  const [message, setMessage] = useState("");
+
+  // message state를 CComp에 전달하기
+  // 1. context 만들기: createContext();
+  // 2. context에 state 넣기: <Context.Provider value={state}></Context.Provider>
+  // 3. tree(하위 컴포넌트)안에 사용하기
+
   return (
     <div>
-      <SomeComp onClick={() => setMessage("다른 메세지!")} />
-      <OtherComp text={message} />
+      <Button onClick={() => setMessage("바꾼 메세지!!")}>버튼</Button>
+      <MessageContext.Provider value={message}>
+        <AComp />
+      </MessageContext.Provider>
     </div>
   );
 }
 
+// 1.Context 만들기
+// context 이름은 ...Context
+// 변수명은 대문자로 시작하고 Context로 끝남.
+const MessageContext = createContext(null);
 export default App;

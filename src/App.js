5d2343781c8f4b91910cd6b5c8314039702fb2c8
jay sung <namic123@naver.com>
2023-11-07 15:43:00 +0900

    react axios,useEffect로 서버사이드(localhost:8080)와의 상호작용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { Button, Text } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function App(props) {
-  const [number, setNumber] = useState(0);
-  console.log("useEffect 밖의 로그"); // 리렌더링 시에 계속 호출
-  // useEffect
-  // 컴포넌트 외부시스템과 작업할 때
-  // 주로 ajax로 데이터 가져올 때
-
-  // 1번째 인자: 실행할 함수
-  // 2번째 인자: 1번째 인자를 실행시키는 값, 빈 배열이면 초기 렌더링 때만 실행됨.
-  // 즉, 리렌더링 시에는 실행되지 않는다.
   useEffect(() => {
-    console.log("이펙트의 1번째 파라미터 함수 실행됨.", number);
+    axios
+      .get("/api/main1/sub1")
+      .then((response) => response.data)
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
   }, []);
-  return (
-    <div>
-      <Button onClick={() => setNumber(number + 1)}>증가</Button>
-      <Text>{number}</Text>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;

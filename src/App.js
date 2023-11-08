266f3341320075c5d5746eb44c9092dbd61193ca
jay sung <namic123@naver.com>
2023-11-08 16:13:20 +0900

    useNavigate 사용. 새로고침 없이 컴포넌트의 이동

--- src/App.js
+++ src/App.js
@@ -6,28 +6,35 @@ import {
   Outlet,
   Route,
   RouterProvider,
+  useNavigate,
 } from "react-router-dom";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Button, Flex } from "@chakra-ui/react";
 
 function HomeComponent() {
+  // 경로 이동시 useNavigate hook 사용 해야함.
+  const navigate = useNavigate(); // 새로고침 없이 컴포넌트의 이동을 가능하게 하는 메서드(Link Component와 유사)
+  // js 코드간 이동이 필요할 떄는 LinkComponent 사용
+  // view의 이동은 useNavigate 사용
   return (
     <>
       <Box>
-        <Flex>
-          {/*          <Box>
-            react-router 사용시 a 태그를 사용하지 않을 것
-            <a href="/apath">에이로 가기 </a>
+        <Flex gap={"10px"}>
+          {/* 경로 이동시 js코드를 그냥 쓰면 안됨 */}
+          <Box>
+            <Button onClick={() => (window.location.href = "/apath")}>
+              a로 가기
+            </Button>
           </Box>
           <Box>
-            <a href="/bpath"> 비로 가기</a>
-          </Box>*/}
-
-          {/* 대신 react-router에서 제공하는 Link Component 사용*/}
-          <Box>
-            <Link to={"apath"}>에이로 가기</Link>
+            <Button onClick={() => (window.location.href = "/bpath")}>
+              b로 가기
+            </Button>
           </Box>
           <Box>
-            <Link to={"bpath"}>에이로 가기</Link>
+            <Button onClick={() => navigate("/apath")}>a로 가기</Button>
+          </Box>
+          <Box>
+            <Button onClick={() => navigate("/bpath")}>b로 가기</Button>
           </Box>
         </Flex>
         <Outlet />

dde84232db7ada97effdb39ae6fa23d3f9699d01
jay sung <namic123@naver.com>
2023-11-08 11:35:05 +0900

    useEffect 활용, 프론트에서 id로 서버에 요청을 보내고 백에서 직원 엔티티를 응답해주는 예제

--- src/App.js
+++ src/App.js
@@ -3,27 +3,27 @@ import { Box, Select, Spinner, Text } from "@chakra-ui/react";
 import axios from "axios";
 
 function App(props) {
-  const [customerId, setCustomerId] = useState(0);
-  const [customer, setCustomer] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-
+  // 직원번호를 선택하면 직원의 이름이 출력
+  // /api/main1/sub5?id=5
+  // spring boot의 메소드도 작성
+  const [employeeId, setEmployeeId] = useState(0);
+  const [employee, setEmployee] = useState(null);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    setIsLoading(true);
+    setLoading(true);
     axios
-      .get("/api/main1/sub4?id=" + customerId)
+      .get("/api/main1/sub5?id=" + employeeId)
       .then((response) => response.data)
-      .then((data) => setCustomer(data))
-      .catch((error) => setCustomer(null))
-      .finally(() => setIsLoading(false));
-  }, [customerId]);
-
+      .then((data) => setEmployee(data))
+      .catch((error) => setEmployee(null))
+      .finally(setLoading(false));
+  }, [employeeId]);
   return (
     <div>
       <Select
-        placeholder="고객 번호를 선택하세요"
-        onChange={(e) => setCustomerId(e.target.value)}
+        placeholder="직원의 번호를 선택하세요."
+        onChange={(e) => setEmployeeId(e.target.value)}
       >
-        {/*option[value=$]{$}*10*/}
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -36,26 +36,16 @@ function App(props) {
         <option value="10">10</option>
       </Select>
       <Box>
-        {isLoading === true ? (
+        {loading === true ? (
           <Spinner />
-        ) : customer === null ? (
-          <Text>존재하지 않는 고객입니다.</Text>
+        ) : employee === null ? (
+          <Text>해당 직원은 존재하지 않습니다.</Text>
         ) : (
-          <Text> 고객 이름 : {customer.name}</Text>
+          <Text>
+            직원의 이름: {employee.lastName} {employee.firstName}, 생일은:
+            {employee.birthDate}
+          </Text>
         )}
-
-        {/* truthy falsy 표현식
-        {isLoading && <Spinner />}
-
-        {isLoading || (
-          <>
-            {customer === null ? (
-              <Text>조회한 고객이 없습니다. 다른 번호를 선택해주세요.</Text>
-            ) : (
-              <Text>고객 이름 : {customer.name}</Text>
-            )}
-          </>
-        )}*/}
       </Box>
     </div>
   );
